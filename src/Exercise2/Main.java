bc6cbcb0630dbd7d4bdccb528fad92e5c0a78821
CorinaIoanaG <corinaghete@gmail.com>
2022-10-07 12:47:33 +0300

    Second Commit

--- src/Exercise2/Main.java
+++ src/Exercise2/Main.java
@@ -1,17 +1,59 @@
 package Exercise2;
 
+import java.util.*;
+
 public class Main {
+    static List<Person> persons = new ArrayList<>();
 
-    public void main(String[] args) {
-        Person person1 = new Person("Ana", 45, "brown");
-        Person person2 = new Person("Andrei", 23, "dark");
-        Person person3 = new Person("Diana", 37, "blonde");
-        Person person4 = new Person("Raluca", 33, "brown");
-        Person person5 = new Person("Radu", 28, "dark");
-        Person person6 = new Person("Tudor", 25, "brown");
-        Person person7 = new Person("Alina", 38, "blonde");
-        for(Person element : Person) {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        persons.add(new Person("Ana", 38, "brown"));
+        persons.add(new Person("Andrei", 28, "dark"));
+        persons.add(new Person("Diana", 37, "blonde"));
+        persons.add(new Person("Raluca", 33, "brown"));
+        persons.add(new Person("Radu", 28, "dark"));
+        persons.add(new Person("Tudor", 25, "brown"));
+        persons.add(new Person("Alina", 38, "blonde"));
+        //2.1
+        for (Person person : persons) {
+            System.out.printf("%s ", person.name());
         }
+        System.out.println();
+        //2.2
+        Map<String, Integer> personsNameAge = new HashMap<>();
+        for (Person person : persons) {
+            personsNameAge.put(person.name(), person.age());
+        }
+        System.out.println(personsNameAge);
+        //2.3
+        System.out.print("Enter the minimum age ");
+        System.out.println(personsOlderThan(scanner.nextInt()));
+        //2.4
+        System.out.println(personsGroupedByHairColour());
+
+    }
+
+    private static List<Person> personsOlderThan(Integer minAge) {
+        List<Person> result = new ArrayList<>();
+        if (persons.size() > 0 && minAge != null) {
+            for (Person person : persons) {
+                if (person.age() > minAge) {
+                    result.add(new Person(person.name(), person.age(), person.hairColour()));
+                }
+            }
+        }
+        return result;
+    }
+
+    private static Map<String, List<String>> personsGroupedByHairColour() {
+        Map<String, List<String>> result = new HashMap<>();
+        if (persons.size() > 0) {
+            for (Person person : persons) {
+                if (!result.containsKey(person.hairColour())) {
+                    result.put(person.hairColour(), new ArrayList<>());
+                }
+            }
+        }
+        return result;
     }
 }
